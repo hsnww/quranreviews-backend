a68cf74c139cd3bcff3d354f7e1ea6ee9d716777
Hassan Al ZAhrani <hsnwww@gmail.com>
2025-09-10 17:08:48 +0300

    correct config cross file for production

--- config/cors.php
+++ config/cors.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 return [
     'paths' => [
         'api/*',
@@ -23,4 +23,33 @@ return [
 
     'supports_credentials' => true,
 ];
+*/
 
+return [
+    'paths' => [
+        'api/*',
+    ],
+
+    'allowed_methods' => ['*'],
+
+    // الدومينات المسموح بها (Production)
+    'allowed_origins' => [
+        'https://www.quranreview.app',
+        'https://quranreview.app',
+        'http://quran-review.test:3000',
+    ],
+
+    // السماح بروابط Preview من Vercel
+    'allowed_origins_patterns' => [
+        '#^https://.*\.vercel\.app$#',
+    ],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    // نستخدم Bearer Token، لذلك اجعلها false
+    'supports_credentials' => false,
+];
